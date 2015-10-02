0261b989e97d7d3bd798f0c0309e9a836860dcc9
Augusto <augf87@gmail.com>
2015-10-02 16:17:50 -0300

    Refactor gulpfile with gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,68 @@
 var gulp = require('gulp');
 var path = require('path');
 var del = require('del');
-var concat = require('gulp-concat');
-var merge = require('merge-stream');
-var sourcemaps = require('gulp-sourcemaps');
-var less = require('gulp-less');
-var rename = require('gulp-rename');
-var config = require('./gulp.config.js');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
 var argv = require('yargs').argv;
-var gulpif = require('gulp-if');
-var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-minify-css');
-var ngAnnotate = require('gulp-ng-annotate');
-var debug = require('gulp-debug');
-var jshint = require('gulp-jshint');
-var gulpFilter = require('gulp-filter');
+var config = require('./gulp.config.js');
+var $ = require('gulp-load-plugins')();
+
 
 gulp.task('watch', ['build'], function () {
-  gulp.watch('src/**/*.js', ['buildJs']);
-  gulp.watch('src/**/*.less', ['buildStyles']);
-  gulp.watch('src/**/*.html', ['buildOther']);
+  gulp.watch('src/**/*.js', ['build:js']);
+  gulp.watch('src/**/*.less', ['build:styles']);
+  gulp.watch('src/**/*.html', ['build:other']);
 });
 
 gulp.task('clean', function(cb) {
   del(['build-dev', 'build-dist'], cb());
 });
 
-gulp.task('build', ['buildStyles', 'buildJs', 'buildOther']);
-  gulp.task('buildJs', ['buildJsPopup', 'buildJsBackground']);
+gulp.task('build', ['build:styles', 'build:js', 'build:other', 'build:finish']);
+  gulp.task('build:js', ['build:js:popup', 'build:js:background']);
 
-gulp.task('buildStyles', function(cb) {
+gulp.task('build:styles', function(cb) {
   return gulp.src(config.styles)
-    .pipe(less({
+    .pipe($.less({
       paths: [ path.join(__dirname, 'bower_components') ]
     }))
-    .pipe(gulpif(isDist(), minifyCss()))
-    .pipe(rename('extension.css'))
+    .pipe($.if(isDist(), $.minifyCss()))
+    .pipe($.rename('extension.css'))
     .pipe(gulp.dest(buildPath('css')));
 });
 
-gulp.task('buildJsPopup', function() {
-  var filter = gulpFilter(['src/**/*.js', '!src/js/popup/ga.js'], {restore: true});
+gulp.task('build:js:popup', function() {
+  var filter = $.filter(['src/**/*.js', '!src/js/popup/ga.js'], {restore: true});
 
   return gulp.src(filterFiles(config.scripts.popup), {base: '.'})
     .pipe(filter)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('default'))
     .pipe(filter.restore)
-    .pipe(concat('popup.js'))
-    .pipe(ngAnnotate())
-    .pipe(gulpif(isDist(), uglify()))
+    .pipe($.concat('popup.js'))
+    .pipe($.ngAnnotate())
+    .pipe($.if(isDist(), $.uglify()))
     .pipe(gulp.dest(buildPath('js')));
 });
 
-gulp.task('buildJsBackground', function() {
+gulp.task('build:js:background', function() {
   return gulp.src(config.scripts.background)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('background.js'))
-    .pipe(browserify({
-      insertGlobals : true,
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('default'))
+    .pipe($.concat('background.js'))
+    .pipe($.browserify({
+      insertGlobals: true,
       debug: isDev()
     }))
-    .pipe(gulpif(argv.dist, uglify()))
+    .pipe($.if(argv.dist, $.uglify()))
     .pipe(gulp.dest(buildPath('js')));
 });
 
-gulp.task('buildOther', function(cb) {
+gulp.task('build:other', function(cb) {
   return gulp.src(config.other, {base: 'src'})
     .pipe(gulp.dest(buildPath()));
+});
+
+gulp.task('build:finish', function(cb) {
 });
 
 function buildPath(path) {
